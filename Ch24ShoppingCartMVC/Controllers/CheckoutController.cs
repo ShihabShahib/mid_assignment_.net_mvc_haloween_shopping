ecef4ee6aabfb020bacc16bac162b5346823cdee
ShihabShahib <shahibshihab@gmail.com>
2020-08-03 00:35:27 +0600

    order_page_viewadded

---
+++ Ch24ShoppingCartMVC/Controllers/CheckoutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ch24ShoppingCartMVC.Controllers
+{
+    public class CheckoutController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
